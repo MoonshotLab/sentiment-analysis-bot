634d22d1382ccd6f5518180ef756c752ba5e3fb0
Preston Richey <prichey@barkleyus.com>
2017-12-08 18:19:39 -0600

    Init

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+
+const bodyParser = require('body-parser');
+const autoReap = require('multer-autoreap');
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(autoReap);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log('Server running on port ' + port);
+});
+
+app.use('/', require('./routes/index'));
+app.use('/process', require('./routes/process'));
+app.use('*', (req, res) => {
+  res.redirect('/');
+});
+
+module.exports = app;
---
+++ routes/process.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const router = express.Router();
+
+const fs = require('fs-extra');
+const path = require('path');
+const ffmpeg = require('fluent-ffmpeg');
+const upload = require('multer')({ dest: '/tmp/' });
+const speech = require('@google-cloud/speech');
+
+const indico = require('indico.io'); // text analysis
+indico.apiKey = process.env.INDICO_API_KEY;
+
+const speechClient = new speech.SpeechClient();
+
+const uploadFieldSpec = [
+  {
+    name: 'data',
+    maxCount: 1
+  }
+];
+
+const googleCloudUploadConfig = {
+  encoding: 'FLAC',
+  languageCode: 'en-US'
+};
+
+function asyncConvertWebmToFlac(inPath, outPath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg(inPath)
+      .audioCodec('flac')
+      .output(outPath)
+      .on('end', resolve)
+      .on('error', reject)
+      .run();
+  });
+}
+
+function asyncGetTranscriptionFromFlac(flacPath) {
+  return new Promise((resolve, reject) => {
+    const audio = {
+      content: fs.readFileSync(flacPath).toString('base64')
+    };
+
+    const request = {
+      config: googleCloudUploadConfig,
+      audio: audio
+    };
+
+    speechClient
+      .recognize(request)
+      .then(data => {
+        const response = data[0];
+        const transcription = response.results
+          .map(result => result.alternatives[0].transcript)
+          .join('\n');
+        resolve(transcription);
+      })
+      .catch(err => reject);
+  });
+}
+
+function removeFlacFile(path) {
+  fs
+    .remove(path)
+    .then(() => {
+      console.log('flac file removed');
+    })
+    .catch(e => {
+      console.log('error removing flac file', e);
+    });
+}
+
+router.post('/', upload.fields(uploadFieldSpec), (req, res) => {
+  if (!!req.files && !!req.files.data && req.files.data.length > 0) {
+    const blob = req.files.data[0];
+
+    const webmFilename = path.parse(blob.filename).name;
+    const webmPath = blob.path;
+    const webmDir = blob.destination;
+
+    const flacPath = path.join(webmDir, `${webmFilename}.flac`);
+
+    let transcription = '';
+
+    asyncConvertWebmToFlac(webmPath, flacPath)
+      .then(() => {
+        return asyncGetTranscriptionFromFlac(flacPath);
+      })
+      .then(transcription => {
+        transcription = transcription.trim();
+        if (!!transcription && transcription.length > 0) {
+          indico
+            .emotion(transcription)
+            .then(emotions => {
+              // emotions is object
+              res.status(200).send({
+                transcription: transcription,
+                emotions: emotions
+              });
+            })
+            .catch(e => {
+              console.log('Error getting text emotion analysis', e);
+              res.status(200).send({
+                transcription: transcription,
+                emotions: null
+              });
+            });
+        } else {
+          console.log('could not transcribe');
+          res.sendStatus(400);
+        }
+      })
+      .catch(e => {
+        console.log(e);
+        res.sendStatus(500);
+      })
+      .finally(() => {
+        // remove flac file
+        removeFlacFile(flacPath);
+      });
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+module.exports = router;
