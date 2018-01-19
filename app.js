046827abc0b436db123c689caa47a2942df13b75
Preston Richey <prichey@barkleyus.com>
2018-01-19 12:34:43 -0600

    Set ffmpeg path env var

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const bodyParser = require('body-parser');
 const autoReap = require('multer-autoreap');
 const path = require('path');
 
+// set ffmpeg path
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+process.env.FFMPEG_PATH = ffmpegPath;
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
